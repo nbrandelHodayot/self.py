4e40a4f08675433f21a668fe8a1313292baf072c
nbrandelHodayot <82374717+nbrandelHodayot@users.noreply.github.com>
2021-04-12 15:51:32 +0300

    Created using Colaboratory

---
+++ Hangman_1_4_1.py
@@ -0,0 +1,79 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "Hangman 1.4.1",
+      "provenance": [],
+      "collapsed_sections": [],
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/nbrandelHodayot/self.py/blob/main/Hangman_1_4_1.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "metadata": {
+        "id": "wFzVV_tUb6dC",
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "outputId": "85e9f056-c8dc-4e9d-c9b7-032f774dc242"
+      },
+      "source": [
+        "import random\n",
+        "print (\"\"\"  _    _                                         \n",
+        " | |  | |                                        \n",
+        " | |__| | __ _ _ __   __ _ _ __ ___   __ _ _ __  \n",
+        " |  __  |/ _` | '_ \\ / _` | '_ ` _ \\ / _` | '_ \\ \n",
+        " | |  | | (_| | | | | (_| | | | | | | (_| | | | |\n",
+        " |_|  |_|\\__,_|_| |_|\\__, |_| |_| |_|\\__,_|_| |_|\n",
+        "                      __/ |                      \n",
+        "                     |___/\"\"\")\n",
+        "random.randint(5, 10)"
+      ],
+      "execution_count": 4,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "text": [
+            "  _    _                                         \n",
+            " | |  | |                                        \n",
+            " | |__| | __ _ _ __   __ _ _ __ ___   __ _ _ __  \n",
+            " |  __  |/ _` | '_ \\ / _` | '_ ` _ \\ / _` | '_ \\ \n",
+            " | |  | | (_| | | | | (_| | | | | | | (_| | | | |\n",
+            " |_|  |_|\\__,_|_| |_|\\__, |_| |_| |_|\\__,_|_| |_|\n",
+            "                      __/ |                      \n",
+            "                     |___/\n"
+          ],
+          "name": "stdout"
+        },
+        {
+          "output_type": "execute_result",
+          "data": {
+            "text/plain": [
+              "8"
+            ]
+          },
+          "metadata": {
+            "tags": []
+          },
+          "execution_count": 4
+        }
+      ]
+    }
+  ]
+}
